4ee17c805a4abf512e7fb61e945d624889ae60e8
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-04-19 00:06:54 +0100

    Added plist loading.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def find_duplicates(filename):
-    print(f"Finding duplicate tracks in {filename}")
+import re
+import argparse
+import sys
+from matplotlib import pyplot
+import plistlib
+import numpy as np
 
 
-find_duplicates("library.xml")
+def load_plist(filename):
+    with open(filename, 'rb') as f:
+        plist = plistlib.load(f)
+        return plist
+
+
+def find_duplicates(plist):
+    print(f"Finding duplicate tracks in {plist}")
+
+
+filename = "library.xml"
+plist = load_plist(filename)
+find_duplicates(plist)
