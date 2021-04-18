6b1c1fd09c7767c29f7710f7cacdcb85f5562fff
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-04-19 00:25:40 +0100

    Added duplicate function.

--- src/main.py
+++ src/main.py
@@ -13,9 +13,26 @@ def load_plist(filename):
 
 
 def find_duplicates(plist):
-    print(f"Finding duplicate tracks in {plist}")
+    # print(f"Finding duplicate tracks in {plist}")
+    tracks = plist['Tracks']
+    names = {}
+    for (track_id, track) in tracks.items():
+        try:
+            name = track['Name']
+            duration = track['Total Time'] // 1000
+            if name in names:
+                if duration == names[name][0]:
+                    count = names[name][1]
+                    names[name] = (duration, count + 1)
+            else:
+                names[name] = (duration, 1)
+        except:
+            pass
+    for (name, (time, count)) in names.items():
+        if count > 1:
+            print(f'{name}\t{count}')
 
 
-filename = "res/library.xml"
+filename = 'res/library.xml'
 plist = load_plist(filename)
 find_duplicates(plist)
