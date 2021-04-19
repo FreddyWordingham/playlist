c809cfaea1ea4d6c7991e267ce9cb95bfca555c4
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-04-19 01:41:23 +0100

    Added command line interface.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import find_common
 import plot_stats
 
 
+import argparse
+
+
 # Load an ITunes plist file.
 def load_plist(filename):
     import plistlib
@@ -11,9 +14,27 @@ def load_plist(filename):
         return plist
 
 
-find_duplicates.run(load_plist("res/library.xml"), "dups.txt")
-find_common.run([
-    load_plist("res/library.xml"),
-    load_plist("res/run.xml"),
-], "common.txt")
-plot_stats.run(load_plist("res/library.xml"))
+# Analyses playlist files exported from iTunes.
+def main():
+    parser = argparse.ArgumentParser(
+        description="Analyse iTunes playlist files.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--common', nargs='*', dest='plFiles', required=False)
+    group.add_argument('--stats', dest='plFile', required=False)
+    group.add_argument('--dup', dest='plFileD', required=False)
+
+    args = parser.parse_args()
+    if args.plFiles:
+        lists = []
+        for file in args.plFiles:
+            lists.append(load_plist(file))
+        find_common.run(lists, "common.txt")
+    elif args.plFile:
+        plot_stats.run(load_plist(args.plFile))
+    elif args.plFileD:
+        find_duplicates.run(load_plist(args.plFileD), "dups.txt")
+    else:
+        print("These are not the tracks you are looking for.")
+
+
+main()
